c25e05b1f6c0df53b9018987aee08f5d066c6904
zouyifeng <zouyifeng@aliyun.com>
2017-11-07 23:32:48 +0800

    匹配更多模式

--- utils/voice.js
+++ utils/voice.js
@@ -1,17 +1,30 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
+function timeout (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 const getVoiceUrl = async (url) => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
+  await timeout(300)
+  await page.click('.share_audio_switch')
   const content = await page.content();
   const $ = await cheerio.load(content);
-  const voiceId = await $('mpvoice').attr('voice_encode_fileid');
-  const name = await $('mpvoice').attr('name');
+  const voiceId = await $('audio').attr('src');
+  // const name = await $('mpvoice').attr('name');
+  console.log(voiceId)
+
+  if (voiceId) {
+    voiceId = voiceId.match(/\?mediaid=(.+)/)[1]
+  }
   
   await browser.close();
-  return { url: `https://res.wx.qq.com/voice/getvoice?mediaid=${voiceId}` , name }
+  return { url: `https://res.wx.qq.com/voice/getvoice?mediaid=${voiceId}` , '下载' }
 };
+
+// getVoiceUrl('https://mp.weixin.qq.com/s/llV1wW1hY5M80AoT7_LpZQ')
 
 module.exports = getVoiceUrl
